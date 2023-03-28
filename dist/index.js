f63556a3a738c60ad701a5ff6172c23f1b1c9937
21ulwe <21ulwe@stockholmscience.se>
2023-03-28 10:53:29 +0200

    dist dir fixes

--- dist/index.js
+++ dist/index.js
@@ -9,7 +9,7 @@ dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
 app.get('/', (req, res) => {
-    res.send('Express + TypeScript Server');
+    res.send('Slaktar Server');
 });
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
