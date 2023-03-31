486a08cf6431adf554567a3182f0e74542574c48
AlexanderJernstrom <alexanderjernstrom@gmail.com>
2023-03-31 14:16:49 +0200

    WIP db prompt connection

--- src/controllers/openAIController.ts
+++ src/controllers/openAIController.ts
@@ -1,12 +1,21 @@
 import { Request, Response, Router } from "express";
-import { getImprovedPrompt } from "../services/openAIService";
+import verifyToken from "../middleware/verify";
+import { getImprovedPrompt, getImprovedResult } from "../services/openAIService";
 
 const router = Router();
 
-router.get("/get-improved-prompt", async (req: Request, res: Response) => {
+router.get("/get-improved-prompt", verifyToken, async (req: Request, res: Response) => {
     const prompt = req.query.prompt as string;
-    const improvedPrompt = await getImprovedPrompt(prompt);
+    const improvedPrompt = await getImprovedPrompt(prompt, (req as any).userId);
     return res.json(improvedPrompt);
 })
+
+router.get("/get-improved-answer", verifyToken, async (req: Request, res: Response) => {
+    const prompt = req.query.prompt as string;
+    const improvedResult = await getImprovedResult(prompt, (req as any).userId);
+    return res.json(improvedResult);
+})
+
+
 
 export default router;
--- src/services/openAIService.ts
+++ src/services/openAIService.ts
@@ -1,14 +1,17 @@
 import { Configuration, OpenAIApi } from "openai"
+import { PrismaClient } from "@prisma/client";
 import constants from "../constants/constants";
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
+const prisma = new PrismaClient();
+
 const openai = new OpenAIApi(configuration);
 
 
 
-export const getImprovedPrompt = async (prompt: string) => {
+export const getImprovedPrompt = async (prompt: string, userId: string) => {
     const formattedPrompt = `${constants.basePrompt}${prompt}`;
     const response = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
@@ -18,6 +21,28 @@ export const getImprovedPrompt = async (prompt: string) => {
         }]
     })
 
-    return response.data.choices;
+    const newPrompt = await prisma.prompt.create({
+        data: {
+            input: prompt,
+            output: response.data.choices[0].message?.content || "",
+            model: "gpt-3.5-turbo",
+            tokenCost: "0",
+            userId
+        }
+    })
+
+    return { response: response.data.choices, prompt: newPrompt };
 }
 
+export const getImprovedResult = async (prompt: string, userId: string) => {
+    const response = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{
+            content: prompt,
+            role: "user"
+        }]
+    })
+
+
+    return response.data.choices[0].message;
+}
