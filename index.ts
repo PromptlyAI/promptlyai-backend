64dadde57965f7c459edc3298ab0e56fd6f677d1
AlexanderJernstrom <alexanderjernstrom@gmail.com>
2023-05-05 12:05:02 +0200

    Removed test code

--- index.ts
+++ index.ts
@@ -25,26 +25,6 @@ app.get("/", (req: Request, res: Response) => {
   res.send("PromptlyLabs api");
 });
 
-app.post("/mail", async (req: Request, res: Response) => {
-  const msg = {
-    to: 'alexanderjernstrom@gmail.com', // Change to your recipient
-    from: 'promptlylabs@gmail.com', // Change to your verified sender
-    subject: 'Sending with SendGrid is Fun',
-    text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-  }
-  sgMail
-    .send(msg)
-    .then((response) => {
-      console.log(response)
-      res.send("Success")
-    })
-    .catch((error) => {
-      console.error(error)
-      res.send(error)
-    })
-})
-
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 
